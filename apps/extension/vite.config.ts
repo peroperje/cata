a9d0db89fe7c05d228cd4185de59b3f25dc68585
Petar B <el.pionir@gmail.com>
2026-01-04 14:43:31 +0100

    fixed ts

--- apps/extension/vite.config.ts
+++ apps/extension/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './src/manifest'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
     root: __dirname,
