1246af9f2ebbecf4a92321c0db4c3a1a3aa3e0d0
Petar B <el.pionir@gmail.com>
2026-01-30 11:53:12 +0100

    update version

--- apps/extension/src/manifest.ts
+++ apps/extension/src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export default defineManifest({
     manifest_version: 3,
     name: 'AI Job Auto-Filler',
-    version: '1.0.2',
+    version: '1.0.3',
     permissions: ['storage', 'activeTab', 'scripting', 'sidePanel'],
     action: {
     },
